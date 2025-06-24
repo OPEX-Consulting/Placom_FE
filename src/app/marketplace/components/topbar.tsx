e2b53c4dc31bc3edda194c5243b3872b9b75c85f
Chisom Victor Nteh <chizzyyoung10@gmail.com>
2025-06-24 21:19:48 +0100

    marketplace main page

--- src/app/marketplace/components/topbar.tsx
+++ src/app/marketplace/components/topbar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { cartItems } from "@/lib/data/products"
 import Image from "next/image"
 
 export function Topbar() {
@@ -50,7 +51,7 @@ export function Topbar() {
                 <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 24 24">
                     <path fill="#8AC73C" d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0 1a1 1 0 0 0-1 1a1 1 0 0 0 1 1a1 1 0 0 0 1-1a1 1 0 0 0-1-1m-9-1a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0 1a1 1 0 0 0-1 1a1 1 0 0 0 1 1a1 1 0 0 0 1-1a1 1 0 0 0-1-1M18 6H4.27l2.55 6H15c.33 0 .62-.16.8-.4l3-4c.13-.17.2-.38.2-.6a1 1 0 0 0-1-1m-3 7H6.87l-.77 1.56L6 15a1 1 0 0 0 1 1h11v1H7a2 2 0 0 1-2-2a2 2 0 0 1 .25-.97l.72-1.47L2.34 4H1V3h2l.85 2H18a2 2 0 0 1 2 2c0 .5-.17.92-.45 1.26l-2.91 3.89c-.36.51-.96.85-1.64.85"></path>
                 </svg>
-                <div className="absolute -top-2 right-0 w-5 h-5 flex justify-center items-center bg-white rounded-full text-xs text-secondary font-extrabold">3</div>
+                <div className="absolute -top-2 right-0 w-5 h-5 flex justify-center items-center bg-white rounded-full text-xs text-secondary font-extrabold">{cartItems.length}</div>
             </div>
           <Button
             variant="outline"
